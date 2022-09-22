0d486da1eba9d4892e577781437097e7470a7f12
Pedro Savelis <savelis.pedro@gmail.com>
2022-09-22 20:45:32 -0300

    fix(unit-tests): remove testnet dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -14,32 +14,31 @@ interface Etherscan {
 
 type HardhatUserEtherscanConfig = HardhatUserConfig & Etherscan;
 
-
 const API_URL = process.env.API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserEtherscanConfig = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   solidity: {
-    compilers: [{ version: '0.8.9', settings: {} }],
+    compilers: [{ version: "0.8.9", settings: {} }],
   },
   networks: {
     hardhat: {
       mining: {
         mempool: {
-          order: "fifo"
-        }
-      }
+          order: "fifo",
+        },
+      },
     },
     localhost: {},
-    goerli: {
-      url: API_URL,
-      accounts: [PRIVATE_KEY],
-    },
-    coverage: {
-      url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
-    },
+    // goerli: {
+    //   url: API_URL,
+    //   accounts: [PRIVATE_KEY],
+    // },
+    // coverage: {
+    //   url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
+    // },
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
