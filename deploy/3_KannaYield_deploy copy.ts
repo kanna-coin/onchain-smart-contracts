b1a5e0ae8c09340979837f0b417de32b01077ffe
Pedro Savelis <savelis.pedro@gmail.com>
2022-09-25 12:24:36 -0300

    feat(deploy): add new deploy scripts for presale (refact previous strategy)

--- deploy/3_KannaYield_deploy copy.ts
+++ deploy/3_KannaYield_deploy copy.ts
@@ -1,9 +1,13 @@
 import { ethers } from "hardhat";
 import getKnnToken from "../src/infrastructure/factories/KannaTokenFactory";
+import getKnnTreasurer from "../src/infrastructure/factories/KannaTreasurerFactory";
+import getKnnYield from "../src/infrastructure/factories/KannaYieldFactory";
 
 async function main() {
   const [deployerWallet] = await ethers.getSigners();
   const knnToken = await getKnnToken(deployerWallet);
+  const knnTreasurer = await getKnnTreasurer(deployerWallet, knnToken);
+  const knnYield = await getKnnYield(deployerWallet, knnToken, knnTreasurer);
 }
 
 main()
