0cf2e8c2ed8f3f32bfaf2aa96290ed7cbcdc6474
Rodrigo Azevedo <rodrigo@rwba.dev>
2022-11-11 10:58:27 -0300

    Coverage Readme

---
+++ .solcover.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+module.exports = {
+    istanbulReporter: ['html', 'lcov', 'text', 'json', 'json-summary'],
+    onIstanbulComplete: () => {
+        const json = require('./coverage/coverage-summary.json');
+
+        const { total, ...contracts } = json;
+
+        const formatCoverage = (coverage) => `${coverage?.covered}/${coverage?.total} (${coverage?.pct}%)`;
+
+        let coverageTable = `|Contract|Statements|Branches|Functions|Lines|\n`;
+        coverageTable += `|-|-|-|-|-|\n`;
+
+        for (const [contract, coverage] of Object.entries(contracts)) {
+            coverageTable += `|${contract}|${formatCoverage(coverage.statements)}|${formatCoverage(coverage.branches)}|${formatCoverage(coverage.functions)}|${formatCoverage(coverage.lines)}|\n`;
+        }
+
+        coverageTable += `|**Total**|**${formatCoverage(total.statements)}**|**${formatCoverage(total.branches)}**|**${formatCoverage(total.functions)}**|**${formatCoverage(total.lines)}**|\n`;
+
+        const markdown = `# Coverage\n\n${coverageTable}`;
+
+        fs.writeFileSync('COVERAGE.md', markdown);
+    }
+};
---
+++ COVERAGE.md
@@ -0,0 +1,9 @@
+# Coverage
+
+|Contract|Statements|Branches|Functions|Lines|
+|-|-|-|-|-|
+|contracts/KannaPreSale.sol|47/47 (100%)|32/32 (100%)|15/15 (100%)|47/47 (100%)|
+|contracts/KannaToken.sol|12/12 (100%)|12/12 (100%)|6/6 (100%)|12/12 (100%)|
+|contracts/KannaTreasurer.sol|3/3 (100%)|0/0 (100%)|2/2 (100%)|3/3 (100%)|
+|contracts/KannaYield.sol|80/81 (98.77%)|27/36 (75%)|13/13 (100%)|80/81 (98.77%)|
+|**Total**|**142/143 (99.3%)**|**71/80 (88.75%)**|**36/36 (100%)**|**142/143 (99.3%)**|
